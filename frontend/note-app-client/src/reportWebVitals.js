980cf86eee57becdbf39dc0c1febcbf6e6335598
Teriyaki229 <sudipta.gogoi3536@gmail.com>
2023-09-06 14:47:27 +0530

    misc

---
+++ frontend/note-app-client/src/reportWebVitals.js
@@ -0,0 +1,13 @@
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      });
+    }
+  };
+  
+  export default reportWebVitals;
+  
