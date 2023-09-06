15fed67259e5f7d7b4c11548206ff6c1affc8d0e
Teriyaki229 <sudipta.gogoi3536@gmail.com>
2023-09-06 14:46:53 +0530

    not working

--- frontend/note-app-client/src/components/Notes/ListNotes/ListNotesComponent.jsx
+++ frontend/note-app-client/src/components/Notes/ListNotes/ListNotesComponent.jsx
@@ -16,6 +16,8 @@ const ListNotesComponent = () => {
   });
 
   const [hoveredNoteId, setHoveredNoteId] = useState(null);
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+  const [linkClickableMap, setLinkClickableMap] = useState({});
 
   useEffect(() => {
     NoteService.getNotes()
@@ -36,6 +38,14 @@ const ListNotesComponent = () => {
     return plainText;
   }
 
+  const handleNoteDelete = () => {
+    setShowConfirmDialog(true);
+  };
+
+  const handleNoteEdit = () => {
+    setShowConfirmDialog(true);
+  };
+
   return (
     <div className="homepage">
       <h1 className="text-center-homepage">Your Notes</h1>
@@ -43,22 +53,40 @@ const ListNotesComponent = () => {
         <div className="create-new-note-btn">Create a new note</div>
       </Link>
       <div className="note-container">
+        {showConfirmDialog && (
+          <ConfirmDialogBox
+            dialogueHeadTitle={"Are you sure you want to delete this?"}
+            message={"You cannot recover the note once you delete it!"}
+          />
+        )}
         {note.notes &&
           note.notes.map((note) => (
-            <Link
-              to={`/view/${note.visibleId}`}
-              className="note-preview"
+            <div
               onMouseEnter={() => setHoveredNoteId(note.visibleId)}
               onMouseLeave={() => setHoveredNoteId(null)}
-            >
-              <div key={note.visibleId}>
-                <h3 className="preview-title">{note.title}</h3>
-                {hoveredNoteId === note.visibleId && <NoteOptions />}
-                <p className="preview-content">
-                  {note.content && sanitizeHtmlAndStripHtmlTags(note.content)}
-                </p>
-              </div>
-            </Link>
+            >{
+              <Link to={`/view/${note.visibleId}`} className="note-preview">
+                <div key={note.visibleId}>
+                  <h3 className="preview-title">{note.title}</h3>
+                  {hoveredNoteId === note.visibleId && (
+                    <NoteOptions
+                      onDelete={handleNoteDelete}
+                      onEdit={handleNoteEdit}
+                      onMouseEnter={() => {
+                        setLinkClickableMap(false);
+                      }}
+                      onMouseLeave={() => {
+                        setLinkClickableMap(true);
+                      }}
+                    />
+                  )}
+                  <p className="preview-content">
+                    {note.content && sanitizeHtmlAndStripHtmlTags(note.content)}
+                  </p>
+                </div>
+              </Link>
+              }
+            </div>
           ))}
       </div>
     </div>
