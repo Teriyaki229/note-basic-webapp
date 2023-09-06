5f393a2fcecacaea7c51d71f7bc0139032ef3081
Teriyaki229 <sudipta.gogoi3536@gmail.com>
2023-09-06 14:47:12 +0530

    notworking

--- frontend/note-app-client/src/components/Notes/NoteOptions/NoteOptions.jsx
+++ frontend/note-app-client/src/components/Notes/NoteOptions/NoteOptions.jsx
@@ -1,16 +1,16 @@
 import styles from "./NoteOptions.module.css";
 
-const NoteOptions = ({onDelete, onEdit}) => {
+const NoteOptions = ({onDelete, onEdit, onMouseEnter, onMouseLeave}) => {
   return (
-    <div>
-      <div className={styles.iconTrash}>
+    <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <div className={styles.iconTrash} onClick={onDelete}>
       <div className={styles.trashLid} style={{backgroundColor: '#E5E9EA'}}></div>
         <div className={styles.trashContainer} style={{backgroundColor: '#E5E9EA'}}></div>
         <div className={styles.trashLine1}></div>
         <div className={styles.trashLine2}></div>
         <div className={styles.trashLine3}></div>
       </div>
-      <div className={styles.pencilWrapper}>
+      <div className={styles.pencilWrapper} onClick={onEdit}>
       <div className={styles.pencil} style={{backgroundColor: '#E5E9EA'}}></div>
       </div>
     </div>
